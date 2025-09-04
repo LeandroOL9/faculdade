54bbeed33d14817f5d759638c1edea0639d96ead
Leandro Oliveira <leandro.tsubrasa@gmail.com>
2025-09-04 08:07:11 -0300

    lado cliente

--- segundo_periodo/programação_web/formulario/servidor/index.js
+++ segundo_periodo/programação_web/formulario/servidor/index.js
@@ -11,6 +11,11 @@ app.get('/api', (req, res) => {
   res.send({ ok: true, msg: 'A rota funcionou' });
 });
 
+app.post('/api', (req, res) => {
+  console.log(req.body); // aqui chegam os dados do form
+  res.send('Formulário recebido com sucesso!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server on http://localhost:${PORT}`);
 });
